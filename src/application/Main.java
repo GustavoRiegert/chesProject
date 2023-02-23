af9acaf5589499a70ca9c7e1f4f59baad2b9e485
gustavo.riegert <gustavinhoh908@gmail.com>
2023-02-23 11:00:06 -0300

    Criando tabuleiro e itens de pecas

---
+++ src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Board;
+import chess.ChessMatch;
+import chess.ChessPiece;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+    }
+}
