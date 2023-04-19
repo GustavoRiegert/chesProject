30b9d73325c9ce7d2840821b6a80b2c6dbf33e8b
Gustavo Riegert <gustavinhoh908@gmail.com>
2023-04-19 11:28:32 -0300

    Colorindo o board

--- src/application/UI.java
+++ src/application/UI.java
@@ -12,6 +12,7 @@ public class UI {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
@@ -34,17 +35,32 @@ public class UI {
         for (int i = 0; i < pieces.length; i++){
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++){
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
 
-    private static void printPiece(ChessPiece piece) {
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves){
+        for (int i = 0; i < pieces.length; i++){
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces.length; j++){
+                printPiece(pieces[i][j], possibleMoves[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a b c d e f g h");
+    }
+
+    private static void printPiece(ChessPiece piece, boolean background) {
+
+        if (background){
+            System.out.print(ANSI_GREEN_BACKGROUND);
+        }
 
         if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         } else {
             if (piece.getColor() == Color.WHITE){
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
